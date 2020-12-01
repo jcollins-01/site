e1025e5f3c3996a652778c7b2403c090a020e38a
jcollins-01 <70544932+jcollins-01@users.noreply.github.com>
2020-11-30 18:18:06 -0700

    Additional comments - need prediction methods

--- MLModels/model.js
+++ MLModels/model.js
@@ -1,21 +1,15 @@
 const webcam = new Webcam(document.getElementById("wc"));
 let model;
-let isPredicting = false;
 
 async function loadModel(){ 
-              const model = await tf.loadModel('web_model/model.json');
+              model = await tf.loadModel('web_model/model.json');
               document.getElementById('out').innerHTML += model;
           }
 
 // Ensures that prediction is started once
 function startPredicting() {
-    isPredicting = true;
-    predict();
-}
-
-// Stops predicting when pressed
-function stopPredicting() {
-    isPredicting = false;
+    webcam.capture();
+    //Need to create a predict() method for the model that will use the model's labels to classify the captured image
     predict();
 }
 
@@ -23,7 +17,6 @@ function stopPredicting() {
 async function init() {
     await webcam.setup();
     model = await loadModel();
-    tf.tidy(() => mobilenet.predict(webcam.capture()));
 }
 
 init();
