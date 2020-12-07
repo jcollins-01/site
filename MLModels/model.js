59e1345bf4ecfeac47d541b02169e7e6b194004e
jcollins-01 <70544932+jcollins-01@users.noreply.github.com>
2020-12-07 07:16:29 -0700

    Include prediction statement

--- MLModels/model.js
+++ MLModels/model.js
@@ -8,10 +8,11 @@ async function loadModel(){
 
 // Ensures that prediction is started once
 function startPredicting() {
-    webcam.capture();
+    const inputData = webcam.capture();
     //Need to create a predict() function for the model that will use the model's labels to classify the captured image
     //Maybe save the image captured by the webcam in a variable in order to do so
-    predict();
+    //predict();
+    const prediction = model.predict(inputData);
 }
 
 // Combines everything
