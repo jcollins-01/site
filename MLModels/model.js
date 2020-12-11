cf7700d9f4e0edb55210c3e2ff13b6cdd1133c54
jcollins-01 <70544932+jcollins-01@users.noreply.github.com>
2020-12-11 14:00:04 -0700

    Add documentation

--- MLModels/model.js
+++ MLModels/model.js
@@ -1,16 +1,21 @@
+/**
+*  Uses the webcam to capture an image and actually run the model's prediction on the image it takes in.
+*  Will compile that prediction into user output. Also sets up the webcam and loads the model onto the page.
+*/
 const webcam = new Webcam(document.getElementById("wc"));
 let model;
-
+/**
+*  Loads the model into the webpage from the web_model folder.
+*/
 async function loadModel(){ 
               model = await tf.loadGraphModel('web_model/model.json');
               document.getElementById('out').innerHTML += model;
           }
 
-// Ensures that prediction is started once
+/**
+*  Will start the model's prediction sequence and choose the output with the highest probability.
+*/
 function startPredicting() {
-    //Need to create a predict() function for the model that will use the model's labels to classify the captured image
-    //Maybe save the image captured by the webcam in a variable in order to do so
-    //predict();
   const predictedClass = tf.tidy(() => {
             const inputData = webcam.capture();
             //const activation = mobilenet.predict(img); --Encodings for mobilenet
@@ -27,12 +32,15 @@ function startPredicting() {
         await tf.nextFrame();           
 }
 
-// Combines everything
+/**
+*  Initializes the webcam and loads the model into a variable model.
+*/
 async function init() {
     await webcam.setup();
     model = await loadModel();
 }
 
+//Code to help figure out conversion from Python to Javascript.
 //const xs = tf.tensor2d([[1], [2], [3], [4]], [4, 1]);
 //dictionary in js, where image is replaced by the array above
 //var signature = JSON.parse("signature.json") <-parses through the json file for outputs/inputs
